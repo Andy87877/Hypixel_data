50e2427a544d554ee300f4b6fba8e2f80f787f49
Andy8787(布長) <andy8787main@gmail.com>
2022-05-21 00:03:50 +0800

    json

--- api.py
+++ api.py
@@ -16,4 +16,11 @@ API_KEY = "c90502bb-24ad-4f07-9601-98ad77c60dca"
 name_link = f"https://api.hypixel.net/player?key={API_KEY}&name={name}"
 uuid_link = f"https://api.hypixel.net/player?key={API_KEY}&uuid={uuid_dashed}"
 
-pprint(getInfo(name_link))
+
+pprint(getInfo(uuid_link))
+
+# print (name_link)
+# https://api.hypixel.net/player?key=c90502bb-24ad-4f07-9601-98ad77c60dca&name=Andy8787
+
+with open("Andy8787_Data.json", "w") as file:
+    json.dump(getInfo(uuid_link),file)
